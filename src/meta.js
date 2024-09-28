4651bff7fc7a0ca7dfb2aecb837a724e5e455f11
Luiz Gonzaga Moreira Correia Junior <34272808+mannybr@users.noreply.github.com>
2024-09-28 00:12:31 -0300

    META

---
+++ src/meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Telegram Media Downloader
+// @name:zh-CN   Telegram图片视频下载器
+// @version      1.202
+// @namespace    https://github.com/mannybr/Telegram-Media-Downloader
+// @description  Download images, GIFs, videos, and voice messages on the Telegram webapp from private channels that disable downloading and restrict saving content
+// @description:zh-cn 从禁止下载的Telegram频道中下载图片、视频及语音消息
+// @author       Nestor Qin
+// @license      GNU GPLv3
+// @website      https://github.com/mannybr/Telegram-Media-Downloader
+// @match        https://web.telegram.org/*
+// @match        https://webk.telegram.org/*
+// @match        https://webz.telegram.org/*
+// ==/UserScript==
